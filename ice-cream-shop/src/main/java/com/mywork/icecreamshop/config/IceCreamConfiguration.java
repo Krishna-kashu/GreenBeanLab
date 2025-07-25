84094499767e510abdae901f9eaa4b90a97dbfd5
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-25 18:57:46 +0530

    working on spring framework

---
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/config/IceCreamConfiguration.java
@@ -0,0 +1,12 @@
+package com.mywork.icecreamshop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.mywork.icecreamshop")
+public class IceCreamConfiguration {
+    public IceCreamConfiguration(){
+        System.out.println("no-arg Constructor of IceCreamConfiguration");
+    }
+}
