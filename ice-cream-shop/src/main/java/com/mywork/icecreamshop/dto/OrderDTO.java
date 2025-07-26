d92a1f2c7c94e4ce130fd1df96b61414e96e7923
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-26 22:04:08 +0530

    working on spring framework

---
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/dto/OrderDTO.java
@@ -0,0 +1,15 @@
+package com.mywork.icecreamshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class OrderDTO {
+    private String name;
+    private String flavour;
+    private int quantity;
+    private boolean takeAway;
+    private boolean addOns;
+    private String coupon;
+}
