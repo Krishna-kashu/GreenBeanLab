e8d59fe74d954b75efeb73c2597a92203127b869
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-26 22:03:48 +0530

    working on spring framework

---
+++ dto-lab/src/main/java/com/mywork/dtolab/config/LabInitializer.java
@@ -0,0 +1,22 @@
+package com.mywork.dtolab.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class LabInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{LabConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/submitVehicle", "/submitBook", "/submitEmployee"};
+    }
+}
