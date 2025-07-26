d7bd221a9989fd04129c50e7245b8bb9d03e8a4b
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-26 22:03:54 +0530

    working on spring framework

---
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.mywork.icecreamshop.controller;
+
+import com.mywork.icecreamshop.dto.OrderDTO;
+import com.mywork.icecreamshop.service.IceCreamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OrderController {
+
+    private final IceCreamService iceCreamService;
+
+    @Autowired
+    public OrderController(IceCreamService iceCreamService){
+        this.iceCreamService = iceCreamService;
+        System.out.println("OrderController constructor");
+    }
+    @RequestMapping("/order")
+    public  String onOrder(OrderDTO orderDTO, Model model){
+        model.addAttribute("name", orderDTO.getName());
+
+        double totalAmount = iceCreamService.totalPrice(orderDTO);
+        model.addAttribute("total", totalAmount);
+        System.out.println("ORDER submitted");
+        return "success.jsp";
+    }
+}
