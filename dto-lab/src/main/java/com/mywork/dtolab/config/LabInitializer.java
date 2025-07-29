75c7a60daa00c53a16646b0d153d682a54278e7d
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-29 09:44:18 +0530

    working on dto-Spring

--- dto-lab/src/main/java/com/mywork/dtolab/config/LabInitializer.java
+++ dto-lab/src/main/java/com/mywork/dtolab/config/LabInitializer.java
@@ -3,8 +3,6 @@ package com.mywork.dtolab.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class LabInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
