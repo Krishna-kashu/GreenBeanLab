9c0fba97d1e1ca2b91fade401b9c3098912010ab
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-02 18:54:38 +0530

    working on spring framework

--- ice-cream-shop/src/main/java/com/mywork/icecreamshop/controller/OrderController.java
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/controller/OrderController.java
@@ -1,31 +1,64 @@
 package com.mywork.icecreamshop.controller;
 
 import com.mywork.icecreamshop.dto.OrderDTO;
-import com.mywork.icecreamshop.service.IceCreamService;
+import com.mywork.icecreamshop.service.IceCreamServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class OrderController {
 
-    private final IceCreamService iceCreamService;
-
     @Autowired
-    public OrderController(IceCreamService iceCreamService){
+    private final IceCreamServiceImpl iceCreamService;
+
+    public OrderController(IceCreamServiceImpl iceCreamService){
 
         this.iceCreamService = iceCreamService;
 
         System.out.println("OrderController constructor with arguments");
     }
-    @RequestMapping("/order")
-    public  String onOrder(OrderDTO orderDTO, Model model){
-        model.addAttribute("name", orderDTO.getName());
 
-        double totalAmount = iceCreamService.totalPrice(orderDTO);
-        model.addAttribute("total", totalAmount);
-        System.out.println("ORDER submitted");
-        return "success.jsp";
+    @RequestMapping("/order")
+    public  String onOrder(@Validated OrderDTO orderDTO, Model model, BindingResult bindingResult) throws IOException {
+        System.out.println("onOrder method");
+        System.out.println("dto: " + orderDTO);
+
+        String filename = orderDTO.getMultipartFile().getOriginalFilename();
+
+        byte[] bytes = orderDTO.getMultipartFile().getBytes();
+        Path path = Paths.get("D:\\MyFilesJava\\" + orderDTO.getName() + System.currentTimeMillis());
+        Files.write(path, bytes);
+
+        if (bindingResult.hasErrors()) {
+
+            bindingResult.getFieldErrors().forEach(error -> System.out.println(error.getField() + " : " + error.getDefaultMessage()));
+
+            System.out.println("Invalid Details");
+            model.addAttribute("dto", orderDTO);
+            model.addAttribute("message", "Invalid details");
+            return "Order";
+        }
+
+        if (iceCreamService.validate(orderDTO)) {
+            model.addAttribute("name", orderDTO.getName());
+            double price = iceCreamService.totalPrice(orderDTO);
+            model.addAttribute("price", price);
+            return "success";
+        } else {
+            System.out.println("Invalid Details");
+            model.addAttribute("dto", orderDTO);
+            model.addAttribute("message", "Invalid details");
+            return "Order";
+        }
     }
 }
