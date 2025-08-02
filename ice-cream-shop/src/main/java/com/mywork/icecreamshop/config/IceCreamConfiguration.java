ab1a15eca8c171ccf4f38261d27b3c9d346811f7
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-02 18:54:11 +0530

    working on spring framework

--- ice-cream-shop/src/main/java/com/mywork/icecreamshop/config/IceCreamConfiguration.java
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/config/IceCreamConfiguration.java
@@ -1,13 +1,44 @@
 package com.mywork.icecreamshop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.mywork.icecreamshop")
-public class IceCreamConfiguration {
+public class IceCreamConfiguration implements WebMvcConfigurer {
 
-    public IceCreamConfiguration(){
+    public IceCreamConfiguration() {
         System.out.println("no-arg Constructor of IceCreamConfiguration");
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        Sy @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        System.out.println("addResourceHandlers method");
+//        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+//
+//        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+//    }
+@Bean
+public InternalResourceViewResolver internalResourceViewResolver()
+{
+    InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+
+    internalResourceViewResolver.setPrefix("/");
+    internalResourceViewResolver.setSuffix(".jsp");
+
+    return internalResourceViewResolver;
+}
+@Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+}
 }
