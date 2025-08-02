6a9efcd42f28ba60b7d84a499a392885a0d8979e
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-02 18:54:22 +0530

    working on spring framework

--- ice-cream-shop/src/main/java/com/mywork/icecreamshop/service/IceCreamService.java
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/service/IceCreamService.java
@@ -1,40 +1,8 @@
 package com.mywork.icecreamshop.service;
 
 import com.mywork.icecreamshop.dto.OrderDTO;
-import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Service
-public class IceCreamService {
-
-    private static  final Map<String, Double> flavourPrice = new HashMap<>();
-    static {
-        flavourPrice.put("Vanilla", 45.0);
-        flavourPrice.put("Chocolate", 60.0);
-        flavourPrice.put("Strawberry", 50.0);
-        flavourPrice.put("BlackCurrent", 70.0);
-        flavourPrice.put("Pineapple", 45.0);
+public interface IceCreamService {
+         double totalPrice(OrderDTO orderDTO);
+        boolean validate(OrderDTO orderDTO);
     }
-
-    private static final List<String> validCoupons = Arrays.asList("456ICED", "ABC98H", "ARTY0392H", "FREE093");
-
-    public double totalPrice(OrderDTO orderDTO){
-
-        double total = flavourPrice.get(orderDTO.getFlavour()) * orderDTO.getQuantity();
-
-        if(orderDTO.isAddOns()){
-            total += 20;
-        }
-        if(orderDTO.isTakeAway()){
-            total +=5;
-        }
-        if(validCoupons.contains(orderDTO.getCoupon())){
-            total *=0.9;
-        }
-        return total;
-    }
-}
