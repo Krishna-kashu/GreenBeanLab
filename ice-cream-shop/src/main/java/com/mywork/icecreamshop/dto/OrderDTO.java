8d1069e9403b142bbb20bcab96f1f58de2a5a5a7
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-03 17:27:24 +0530

    working on spring framework validation

--- ice-cream-shop/src/main/java/com/mywork/icecreamshop/dto/OrderDTO.java
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/dto/OrderDTO.java
@@ -2,6 +2,7 @@ package com.mywork.icecreamshop.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Max;
@@ -10,16 +11,16 @@ import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
-
+@NoArgsConstructor
 public class OrderDTO {
-    @Size(min = 3, max = 30)
+    @Size(min = 3, max = 30, message = "name size min = 3, max = 30")
     private String name;
 
-    @Size(min = 3, max = 30)
+    @Size(min = 3, max = 30, message = "flavour size min = 3, max = 30")
     private String flavour;
 
-    @Min(value = 2, message = "minimum is one")
-    @Max(value = 50, message = "minimum is one")
+    @Min(value = 2, message = "minimum is 2")
+    @Max(value = 50, message = "maximum is 50")
     private int quantity;
     private boolean takeAway;
     private boolean addOns;
