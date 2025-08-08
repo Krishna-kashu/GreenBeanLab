1b10a31ee8de705de526eda894960e386a023432
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-08 22:08:06 +0530

    learning jpa

---
+++ shop-app/src/main/java/com/mywork/shop_app/dto/ShopDTO.java
@@ -0,0 +1,20 @@
+package com.mywork.shop_app.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ShopDTO {
+
+    private String shopName;
+
+    private String shopOwner;
+
+    private Integer totalBranch;
+
+    private String ShopType;
+
+    private String email;
+
+    private Long contact;
+}
