c1d89cd0874fa73f8ad46fc01359b901798c1bb5
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-09 22:28:03 +0530

    learning jpa

---
+++ contact_app/src/main/java/com/mywork/contact_app/dto/ContactDTO.java
@@ -0,0 +1,11 @@
+package com.mywork.contact_app.dto;
+
+import lombok.Data;
+
+@Data
+public class ContactDTO {
+    private String name;
+    private String email;
+    private double contact;
+    private String comments;
+}
