d587c40c2fc072b169c3bc40514f95d74cf57283
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-09 22:28:29 +0530

    learning jpa

--- ice-cream-shop/src/main/java/com/mywork/icecreamshop/service/EmailSenderImpl.java
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/service/EmailSenderImpl.java
@@ -3,24 +3,45 @@ package com.mywork.icecreamshop.service;
 import com.mywork.icecreamshop.config.EmailConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailSenderImpl implements  EmailSender{
 
+//    @Autowired
+//    EmailConfiguration emailConfiguration;
+
     @Autowired
-    EmailConfiguration emailConfiguration;
+    private JavaMailSender javaMailSender;
+
+
+//    @Override
+//    public void mailSend(String email, String otp) {
+//
+//        System.out.println("Invoking mailSend ");
+//        System.out.println("mail:"+email+"otp: "+otp);
+//        SimpleMailMessage simpleMailMessage= new SimpleMailMessage();
+//        simpleMailMessage.setTo(email);
+//        simpleMailMessage.setSubject("Otp to login");
+//        simpleMailMessage.setText("Otp for login "+otp);
+//        emailConfiguration.emailSender().send(simpleMailMessage);
+//        System.out.println("Otp sent for :"+email+" : "+otp);
+//    }
 
     @Override
-    public void mailSend(String email, String otp) {
+    public void mailSend(String email, String subject, String message) {
 
         System.out.println("Invoking mailSend ");
-        System.out.println("mail:"+email+"otp: "+otp);
+        System.out.println("Sending email to: " + email);
+
         SimpleMailMessage simpleMailMessage= new SimpleMailMessage();
         simpleMailMessage.setTo(email);
-        simpleMailMessage.setSubject("Otp to login");
-        simpleMailMessage.setText("Otp for login "+otp);
-        emailConfiguration.emailSender().send(simpleMailMessage);
-        System.out.println("Otp sent for :"+email+" : "+otp);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+//added to send gif file
+        javaMailSender.send(simpleMailMessage);
+//        emailConfiguration.emailSender().send(simpleMailMessage);
+        System.out.println("Email sent successfully to " + email);
     }
 }
