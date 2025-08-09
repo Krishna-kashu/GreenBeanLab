d84b83d4e8f9d1c145b5c247d39ac51e967feb6f
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-09 22:28:23 +0530

    learning jpa

--- ice-cream-shop/src/main/java/com/mywork/icecreamshop/service/EmailSender.java
+++ ice-cream-shop/src/main/java/com/mywork/icecreamshop/service/EmailSender.java
@@ -1,5 +1,5 @@
 package com.mywork.icecreamshop.service;
 
 public interface EmailSender {
-     void mailSend(String email, String otp);
+     void mailSend(String email, String subject, String message);
 }
