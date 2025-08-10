f1a3941f47b9c238c1e8381599a1d0cfddc41baa
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-10 21:16:22 +0530

    working on spring jpa

---
+++ news-letter-web/src/main/java/com/mywork/newsletter/dto/NewsLetterDTO.java
@@ -0,0 +1,19 @@
+package com.mywork.newsletter.dto;
+
+import lombok.Data;
+
+@Data
+public class NewsLetterDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Integer age;
+
+    private String Gender;
+
+    private String topic;
+}
---
+++ news-letter-web/src/main/java/com/mywork/newsletter/entity/NewsLetterEntity.java
@@ -0,0 +1,36 @@
+package com.mywork.newsletter.entity;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@Table(name = "news_letter_subscription_table")
+public class NewsLetterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "gender")
+    private String Gender;
+
+    @Column(name = "topic")
+    private String topic;
+}
