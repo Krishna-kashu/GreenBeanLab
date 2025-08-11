c50b3777b20d61d3c7664fb525da6f5e187abb44
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-11 22:57:28 +0530

    working on jsp spring integration  queries

---
+++ product-inquiry-app/src/main/java/com/mywork/productinquiryapp/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.mywork.productinquiryapp.service;
+
+import com.mywork.productinquiryapp.dto.ProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+    boolean valid(ProductDTO dto);
+    List<ProductDTO> getAll();
+}
