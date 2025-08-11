90a366725a5bdc3ed516be5727b9d77aa819dc25
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-11 22:57:40 +0530

    working on jsp spring integration  queries

---
+++ product-inquiry-app/src/main/java/com/mywork/productinquiryapp/repo/ProductRepo.java
@@ -0,0 +1,11 @@
+package com.mywork.productinquiryapp.repo;
+
+import com.mywork.productinquiryapp.dto.ProductDTO;
+import com.mywork.productinquiryapp.entity.ProductEntity;
+
+import java.util.List;
+
+public interface ProductRepo {
+    boolean save(ProductEntity entity);
+    List<ProductEntity> fetchAll();
+}
---
+++ product-inquiry-app/src/main/java/com/mywork/productinquiryapp/repo/ProductRepoImpl.java
@@ -0,0 +1,58 @@
+package com.mywork.productinquiryapp.repo;
+
+import com.mywork.productinquiryapp.entity.ProductEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class ProductRepoImpl implements ProductRepo{
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-work");
+
+    @Override
+    public boolean save(ProductEntity entity) {
+        System.out.println("save method in ProductRepoImpl");
+
+        if (entity!=null) {
+            System.out.println("Repo: " + entity);
+            EntityManager manager = null;
+            EntityTransaction transaction = null;
+            try {
+                manager = emf.createEntityManager();
+                transaction = manager.getTransaction();
+
+                transaction.begin();
+                manager.merge(entity);
+                transaction.commit();
+                System.out.println("saved entity: "+entity);
+
+            }catch (PersistenceException e){
+                if (transaction.isActive()) transaction.rollback();
+                System.out.println("error in save in repo"+e.getMessage());
+            }finally {
+                if (manager!=null) manager.close();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public List<ProductEntity> fetchAll() {
+        List<ProductEntity> list = Collections.emptyList();
+        EntityManager manager = null;
+
+        try {
+            manager = emf.createEntityManager();
+            Query query = manager.createNamedQuery("fetchAll");
+            list = query.getResultList();
+        }catch (PersistenceException e){
+            e.getMessage();
+        }finally {
+            if(manager!=null) manager.close();
+        }
+        return list;
+    }
+}
