535ce84114fc1c281f998a2f75906acc0cff380d
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-14 19:08:32 +0530

    learning jpa and spring

--- shop-app/src/main/java/com/mywork/shop_app/service/ShopService.java
+++ shop-app/src/main/java/com/mywork/shop_app/service/ShopService.java
@@ -2,6 +2,11 @@ package com.mywork.shop_app.service;
 
 import com.mywork.shop_app.dto.ShopDTO;
 
+import java.util.List;
+
 public interface ShopService {
     boolean valid(ShopDTO shopDTO);
+    List<ShopDTO> getAll();
+    ShopDTO getById(int id);
+    boolean mailExits(String email);
 }
--- shop-app/src/main/java/com/mywork/shop_app/service/ShopServiceImpl.java
+++ shop-app/src/main/java/com/mywork/shop_app/service/ShopServiceImpl.java
@@ -6,6 +6,9 @@ import com.mywork.shop_app.repo.ShopRepoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ShopServiceImpl implements  ShopService{
 
@@ -39,5 +42,50 @@ public class ShopServiceImpl implements  ShopService{
 
         shopRepo.save(shopEntity);
         return valid;
+    }
+
+    @Override
+    public List<ShopDTO> getAll() {
+        System.out.println("get All method is running in serviceImpl");
+
+        List<ShopDTO> shopDTOList = null;
+        List<ShopEntity> shopEntityList = shopRepo.findAll();
+
+        shopDTOList = shopEntityList.stream().map(e -> {
+            ShopDTO dto = new ShopDTO();
+            dto.setShopId(e.getShopId());
+            dto.setShopName(e.getShopName());
+            dto.setShopOwner(e.getShopOwner());
+            dto.setTotalBranch(e.getTotalBranch());
+            dto.setShopType(e.getShopType());
+            dto.setEmail(e.getEmail());
+            dto.setContact(e.getContact());
+            return dto;
+        }).collect(Collectors.toList());
+
+        shopEntityList.forEach(e -> System.out.println(e));
+
+        return shopDTOList;
+    }
+
+    @Override
+    public ShopDTO getById(int id) {
+        ShopDTO dto = new ShopDTO();
+        ShopEntity entity = shopRepo.findById(id);
+        dto.setShopId(entity.getShopId());
+        dto.setShopName(entity.getShopName());
+        dto.setShopOwner(entity.getShopOwner());
+        dto.setTotalBranch(entity.getTotalBranch());
+        dto.setShopType(entity.getShopType());
+        dto.setEmail(entity.getEmail());
+        dto.setContact(entity.getContact());
+
+        return dto;
+    }
+
+    @Override
+    public boolean mailExits(String email) {
+        System.out.println("from service Checking if email exists: " + email);
+        return shopRepo.existsByEmail(email);
     }
 }
