205fc835d69c198a7228bc0a38f77a802703582a
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-18 18:40:58 +0530

    working on jsp spring integration

--- product-inquiry-app/src/main/java/com/mywork/productinquiryapp/entity/ProductEntity.java
+++ product-inquiry-app/src/main/java/com/mywork/productinquiryapp/entity/ProductEntity.java
@@ -10,6 +10,11 @@ import javax.persistence.*;
 @RequiredArgsConstructor
 @Table(name = "product_inquiry_table")
 @NamedQuery(name = "fetchAll", query = "select p from ProductEntity p")
+@NamedQuery(name = "checkMail", query = "select p from ProductEntity p where p.email=:email")
+@NamedQuery(name = "deleteById", query = "delete from ProductEntity p where p.id=:id")
+@NamedQuery(name = "updateEntity", query = "update ProductEntity p set p.fullName=:fullName, " +
+        "p.email=:email, p.phone=:phone, p.productName=:productName, p.inquiryType=:inquiryType, " +
+        "p.message=:message where p.id=:id")
 public class ProductEntity {
 
     @Id
