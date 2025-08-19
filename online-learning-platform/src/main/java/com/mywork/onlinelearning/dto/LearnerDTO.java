74963a788dbbadb775dea91990345119cc240d99
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-19 14:44:25 +0530

    working on login and register of online-learning-platform

---
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/dto/LearnerDTO.java
@@ -0,0 +1,28 @@
+package com.mywork.onlinelearning.dto;
+
+import lombok.Data;
+
+@Data
+public class LearnerDTO {
+
+    private Integer learnerId;
+
+    private String name;
+
+    private String gender;
+
+    private String dob;
+
+    private String email;
+
+    private Long phone;
+
+    private String state;
+
+    private String address;
+
+    private String password;
+
+    private String confirmPassword;
+
+}
