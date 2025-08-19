33d350d5320b2352470ca381a9a25974d73b2033
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-19 14:44:33 +0530

    working on login and register of online-learning-platform project

---
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/entity/LearnerEntity.java
@@ -0,0 +1,48 @@
+package com.mywork.onlinelearning.entity;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NamedQuery(name = "checkMail", query = "select l.email from LearnerEntity l where l.email=:email and l.isActive=true")
+@NamedQuery(name = "checkPhone", query = "select l.phone from LearnerEntity l where l.phone=:phone and l.isActive=true")
+@NamedQuery(name = "getByEmail", query = "select l from LearnerEntity l where l.email=:email and l.isActive=true")
+@Table(name = "online_learning_platform")
+public class LearnerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "learner_id")
+    private Integer learnerId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "dob")
+    private String dob;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private Long phone;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+}
