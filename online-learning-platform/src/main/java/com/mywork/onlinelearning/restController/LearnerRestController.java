8dffa3f0cda88dd17dfccb882d26c5aa964cdfe3
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-19 14:44:54 +0530

    working on login and register of online-learning-platform project

---
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/restController/LearnerRestController.java
@@ -0,0 +1,42 @@
+package com.mywork.onlinelearning.restController;
+
+import com.mywork.onlinelearning.service.LearnerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class LearnerRestController {
+    @Autowired
+    private LearnerServiceImpl service;
+    public LearnerRestController(){
+        System.out.println("LearnerRestController constructor");
+    }
+    @GetMapping("mailCheck")
+    public String checkMail(@RequestParam("email") String email){
+        System.out.println("checkMail method in restController");
+        String existingMail = service.checkEmail(email);
+        System.out.println("Email : "+existingMail);
+        if (existingMail==null){
+            return "Email not exists";
+        }
+        else {
+            return "Email Registered";
+        }
+    }
+
+    @GetMapping("phoneCheck")
+    public String checkPhone(@RequestParam("phone") Long phone){
+        System.out.println("checkPhone method in restController");
+        String existingPhone = service.checkPhone(phone);
+        if(existingPhone == null || existingPhone.isEmpty()){
+            return "Phone number not registered";
+        }
+        return "Phone number already registered";
+    }
+
+
+}
