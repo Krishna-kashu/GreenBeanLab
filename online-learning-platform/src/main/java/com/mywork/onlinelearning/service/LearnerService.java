2ffd71cbbfba071c49c78266d21183fb4eb9476a
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-20 23:41:36 +0530

    working on login and register of online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/service/LearnerService.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/service/LearnerService.java
@@ -4,7 +4,15 @@ import com.mywork.onlinelearning.dto.LearnerDTO;
 
 public interface LearnerService {
     boolean valid(LearnerDTO dto);
+
     String checkEmail(String email);
     String checkPhone(Long phone);
     LearnerDTO getUserDTO(String email, String password);
+
+    String generateOTP(String email);
+
+    boolean verifyOTP(String email, String otp);
+
+    boolean resetPassword(String email, String newPassword, String confirmPassword);
+
 }
