283e613c919e2e5020290c14c54c2c6bd25eb876
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-25 10:51:35 +0530

    working on login and register of online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/repo/LearnerRepo.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/repo/LearnerRepo.java
@@ -5,6 +5,7 @@ import com.mywork.onlinelearning.entity.LearnerEntity;
 public interface LearnerRepo {
     boolean save(LearnerEntity entity);
     String checkMail(String email);
-    String checkPhone(Long phone);
+    Long checkPhone(Long phone);
     LearnerEntity getByMail(String email);
+    void updateLoginCount(LearnerEntity learner);
 }
