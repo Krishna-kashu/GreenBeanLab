cc98ef2bdce671afa171c95ba39184874183e134
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-25 10:51:39 +0530

    working on login and register of online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/entity/LearnerEntity.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/entity/LearnerEntity.java
@@ -1,18 +1,22 @@
 package com.mywork.onlinelearning.entity;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
 @Data
-@RequiredArgsConstructor
-@NamedQuery(name = "checkMail", query = "select l.email from LearnerEntity l where l.email=:email and l.isActive=true")
-@NamedQuery(name = "checkPhone", query = "select l.phone from LearnerEntity l where l.phone=:phone and l.isActive=true")
-@NamedQuery(name = "getByEmail", query = "select l from LearnerEntity l where l.email=:email and l.isActive=true")
+@Entity
 @Table(name = "online_learning_platform")
+@NamedQueries({
+        @NamedQuery( name = "checkMail", query = "select l.email from LearnerEntity l where l.email=:email and l.isActive=true"
+        ),
+        @NamedQuery( name = "checkPhone", query = "select l.phone from LearnerEntity l where l.phone=:phone and l.isActive=true"
+        ),
+        @NamedQuery( name = "getByEmail", query = "select l from LearnerEntity l where l.email=:email and l.isActive=true"
+        ),
+})
 public class LearnerEntity {
 
     @Id
@@ -20,14 +24,14 @@ public class LearnerEntity {
     @Column(name = "learner_id")
     private Integer learnerId;
 
-    @Column(name = "name")
+    @Column(name = "name",nullable = false)
     private String name;
 
     @Column(name = "gender")
     private String gender;
 
     @Column(name = "dob")
-    private String dob;
+    private LocalDate dob;
 
     @Column(name = "email")
     private String email;
@@ -47,16 +51,9 @@ public class LearnerEntity {
     @Column(name = "is_active")
     private Boolean isActive;
 
-    @Column(name="otp")
-    private String otp;
+    @Column(name = "login_count")
+    private Integer loginCount;
 
-    @Column(name="otp_expiry")
-    private LocalDateTime otpExpiry;
-
-    @Column(name="reset_flag")
-    private Integer resetFlag;
-
-    @Column(name="first_login_done")
-    private Boolean firstLoginDone = false;
-
+    @Column(name = "lock_time", columnDefinition = "DATETIME")
+    private LocalDateTime lockTime;
 }
