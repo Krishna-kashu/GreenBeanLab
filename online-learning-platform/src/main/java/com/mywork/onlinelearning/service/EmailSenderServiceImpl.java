5ad7ed8c448a2a465100d88033a194dc20910eb0
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-25 10:51:15 +0530

    working on login and register of online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/service/EmailSenderServiceImpl.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/service/EmailSenderServiceImpl.java
@@ -6,25 +6,34 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmailSenderServiceImpl implements EmailSenderService{
+public class EmailSenderServiceImpl implements EmailSenderService {
 
     @Autowired
     private EmailConfiguration emailConfiguration;
 
-    public EmailSenderServiceImpl(){
+    public EmailSenderServiceImpl() {
         System.out.println("EmailSenderServiceImpl constructor");
     }
 
     @Override
-    public void sendOTP(String email, String otp) {
+    public boolean sendOTP(String email, String otp) {
         System.out.println("Invoking sendOTP method in EmailSenderServiceImpl");
-        System.out.println("mail: "+email+"otp: "+otp);
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(email);
-        simpleMailMessage.setSubject("Online Learning Platform");
-        simpleMailMessage.setText("OTP for login Online learning Platform "+otp);
-        emailConfiguration.javaMailSender().send(simpleMailMessage);
-        System.out.println("OTP sent for: "+email+" : "+otp);
+        System.out.println("mail: " + email + " otp: " + otp);
 
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(email);
+            simpleMailMessage.setSubject("Online Learning Platform");
+            simpleMailMessage.setText("OTP for login Online Learning Platform: " + otp);
+
+            emailConfiguration.javaMailSender().send(simpleMailMessage);
+
+            System.out.println("OTP sent successfully to: " + email + " : " + otp);
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Failed to send OTP to " + email + " due to: " + e.getMessage());
+            return false;
+        }
     }
 }
