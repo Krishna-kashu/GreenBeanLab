7ac280c1608858d7bc150ce97b25437fb1cdcd08
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-26 18:48:06 +0530

    working on online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/repo/LearnerRepoImpl.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/repo/LearnerRepoImpl.java
@@ -39,15 +39,18 @@ public class LearnerRepoImpl implements LearnerRepo{
                 return true;
 
             }catch (PersistenceException e){
+                System.err.println("error in save method :" + e.getMessage());
+
                 if (transaction != null && transaction.isActive()) {
                     transaction.rollback();
                 }
-                System.err.println("error in save method :" + e.getMessage());
                 return false;
             }
             finally {
-                if (manager!=null) manager.close();
-            }
+                if (manager != null && manager.isOpen()) {
+                    manager.close();
+                    System.out.println("EntityManager closed");
+                }            }
         }
         return false;
     }
@@ -59,33 +62,36 @@ public class LearnerRepoImpl implements LearnerRepo{
 
         try {
             manager =emf.createEntityManager();
-            List<String> resultList = manager.createNamedQuery("checkMail")
+            List<String> resultList = manager.createNamedQuery("checkMail", String.class)
                     .setParameter("email", email)
                     .getResultList();
-            if (resultList.isEmpty()) {
-                return null;
-            } else {
-                return resultList.get(0);
-            }
+//            if (resultList.isEmpty()) {
+//                return null;
+//            } else {
+//                return resultList.get(0);
+//            }
+        return resultList.isEmpty() ? null : resultList.get(0);
+
         }catch (PersistenceException e){
             System.err.println("error in checkMail : "+e.getMessage());
             return null;
         }finally {
             if (manager!=null) manager.close();
+            System.out.println("EntityManager closed");
         }
     }
 
     @Override
-    public String checkPhone(Long phone) {
+    public Long checkPhone(Long phone) {
         System.out.println("checkPhone method in repo");
         EntityManager manager = null;
 
         try {
             manager= emf.createEntityManager();
-            Long number = (Long) manager.createNamedQuery("checkPhone")
+            List<Long> list= manager.createNamedQuery("checkPhone", Long.class)
                     .setParameter("phone", phone)
-                    .getSingleResult();
-            return number != null ? String.valueOf(number) : null;
+                    .getResultList();
+            return list.isEmpty() ? null : list.get(0);
 
         }catch (NoResultException e){
             System.err.println("error in checkMail : "+e.getMessage());
@@ -100,10 +106,10 @@ public class LearnerRepoImpl implements LearnerRepo{
     public LearnerEntity getByMail(String email) {
         System.out.println("getByMail method in repo");
         EntityManager manager = null;
-        LearnerEntity entity = null;
         try {
             manager = emf.createEntityManager();
-            entity = (LearnerEntity) manager.createNamedQuery("getByEmail").setParameter("email",email )
+            return manager.createNamedQuery("getByEmail", LearnerEntity.class)
+                    .setParameter("email",email )
                     .getSingleResult();
 
         }catch (NoResultException e) {
@@ -111,9 +117,89 @@ public class LearnerRepoImpl implements LearnerRepo{
             return null;
         }catch (PersistenceException e){
             System.err.println("error in checkMail : "+e.getMessage());
+            return null;
         }finally {
             if (manager!=null) manager.close();
         }
-        return entity;
     }
+
+    @Override
+    public void updateLoginCount(LearnerEntity learner) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.merge(learner);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+    @Override
+    public boolean updateEntity(LearnerEntity entity){
+        EntityManager manager = null;
+        boolean isUpdated = false;
+
+        try{
+            manager = emf.createEntityManager();
+            EntityTransaction  transaction = manager.getTransaction();
+            transaction.begin();
+
+            Integer rows = manager.createNamedQuery("updateDTO")
+                    .setParameter("name", entity.getName())
+                    .setParameter("gender", entity.getGender())
+                    .setParameter("dob", entity.getDob())
+                    .setParameter("phone", entity.getPhone())
+                    .setParameter("state", entity.getState())
+                    .setParameter("address", entity.getAddress())
+                    .setParameter("password", entity.getPassword())
+                    .setParameter("id", entity.getLearnerId())
+                    .executeUpdate();
+            manager.merge(entity);
+            if (rows > 0) {
+                isUpdated = true;
+            }
+            transaction.commit();
+
+        }catch (PersistenceException e){
+            System.out.println("error in updateDTO: "+e.getMessage());
+        }finally {
+            if (manager!=null) manager.close();
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public LearnerEntity findByID(int id) {
+        EntityManager manage= emf.createEntityManager();
+
+        try {
+            return manage.find(LearnerEntity.class, id);
+        }catch (PersistenceException e){
+            System.err.println("error in findByID "+ e.getMessage());
+        }finally {
+            if (manage!=null) manage.close();
+        }
+        return null;
+    }
+
+//
+//    @Override
+//    public boolean updateOtpAndTime(LearnerEntity learner) {
+//        EntityManager em = emf.createEntityManager();
+//        LearnerEntity entity = em.find(LearnerEntity.class, learner.getLearnerId());
+//        if (entity != null) {
+//            entity.setPassword(learner.getPassword());
+//            entity.setLastOtpSentTime(learner.getLastOtpSentTime());
+//            em.getTransaction().begin();
+//            em.merge(entity);
+//            em.getTransaction().commit();
+//            return true;
+//        }
+//        return false;
+//    }
 }
