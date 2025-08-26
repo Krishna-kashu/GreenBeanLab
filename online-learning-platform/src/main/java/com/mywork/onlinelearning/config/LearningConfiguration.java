35d6eec21d5f21d80fce1df0886e3b17f60d06c0
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-26 18:48:11 +0530

    working on online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/config/LearningConfiguration.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/config/LearningConfiguration.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.mywork.onlinelearning")
-public class LearningConfiguration {
+public class LearningConfiguration implements WebMvcConfigurer {
 
     public LearningConfiguration() {
         System.out.println("no-arg constructor in LearningConfiguration");
@@ -26,5 +32,19 @@ public class LearningConfiguration {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         System.out.println("bCryptPasswordEncoder method in configuration");
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("/uploads/");
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/images/");
     }
 }
