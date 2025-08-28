3433fad55f21ad0b55db7f9b22c80d4cda147015
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-28 22:23:15 +0530

    working on Spring, jpa and Ajax

--- passport-seva/src/main/java/com/mywork/passport_seva/controller/PassportController.java
+++ passport-seva/src/main/java/com/mywork/passport_seva/controller/PassportController.java
@@ -5,9 +5,7 @@ import com.mywork.passport_seva.service.PassportServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -60,5 +58,58 @@ public class PassportController {
         List<PassportDTO> passportDTOS = service.getAll();
         model.addAttribute("dtos", passportDTOS);
         return "getAll";
+    }
+
+    @GetMapping("getById")
+    public  String getById(@RequestParam("id") Integer id, Model model){
+
+        System.out.println("getById method is invoked..");
+        PassportDTO dto = service.getById(id);
+
+        model.addAttribute("dto", dto);
+        model.addAttribute("id", id);
+        System.out.println("id is "+id);
+
+        return "entityById";
+    }
+
+    @GetMapping("update")
+    public String edit(@RequestParam("id") Integer id, Model model){
+
+        System.out.println("updateAll method in controller");
+        System.out.println("id: "+id);
+        PassportDTO dto = service.getById(id);
+        System.out.println("dto in update controller: "+dto);
+        model.addAttribute("msg", "updated");
+        model.addAttribute("dto", dto);
+
+        return "edit";
+    }
+
+    @PostMapping("editPassport")
+    public String bugEdit( PassportDTO dto, Model model){
+        System.out.println("bugEdit method in controller");
+
+        String isUpdated = service.updateEntity(dto);
+        System.out.println("updated: "+isUpdated);
+
+        List<PassportDTO> passportDTOList = service.getAll();
+        model.addAttribute("dtos", passportDTOList);
+        return "getAll";
+    }
+
+    @GetMapping("delete/{id}")
+    public String delete(@PathVariable("id") Integer id, Model model){
+
+        System.out.println("delete method in controller");
+
+        String isDeleted = service.deleteById(id);
+        System.out.println("deleted "+ isDeleted);
+
+        model.addAttribute("msg", "delete");
+        List<PassportDTO> passportDTOS = service.getAll();
+        model.addAttribute("bugs", passportDTOS);
+
+        return "redirect:/allDetails";
     }
 }
