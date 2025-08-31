cd18050680ca0fab03e900366b5cbdbb2de96348
Krishna-kashu <krishnavenin8192@gmail.com>
2025-08-31 10:15:47 +0530

    working on Spring, jpa and Ajax

--- passport-seva/src/main/java/com/mywork/passport_seva/repo/PassportRepo.java
+++ passport-seva/src/main/java/com/mywork/passport_seva/repo/PassportRepo.java
@@ -5,11 +5,18 @@ import com.mywork.passport_seva.entity.PassportEntity;
 import java.util.List;
 
 public interface PassportRepo {
+
     boolean save(PassportEntity entity);
+
     List<PassportEntity> findAll();
+
     boolean checkMail(String mail);
+
     boolean checkPhone(long phoneNumber);
+
     boolean deleteById(int id);
+
     boolean updateEntity(PassportEntity entity);
+
     PassportEntity findById(int id);
 }
