eb17e9ab7aef5c2dca266762b7e653a3152edfeb
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-05 22:11:58 +0530

    working on spring mvc project

--- user-management/src/main/java/com/mywork/usermanagement/config/UserConfiguration.java
+++ user-management/src/main/java/com/mywork/usermanagement/config/UserConfiguration.java
@@ -1,13 +1,27 @@
 package com.mywork.usermanagement.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@PropertySource("classpath:application.properties")
 @Configuration
 @ComponentScan(basePackages = "com.mywork.usermanagement")
 public class UserConfiguration {
+
+    @Autowired
+    Environment environment;
+
     public UserConfiguration(){
         System.out.println("no-arg constructor of UserConfiguration");
     }
@@ -19,5 +33,42 @@ public class UserConfiguration {
         resourceViewResolver.setSuffix(".jsp");
 
         return resourceViewResolver;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(){
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+
+        localContainerEntityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+
+        localContainerEntityManagerFactoryBean.setPackagesToScan(new  String[]{"com.mywork.usermanagement.entity"});
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        driverManagerDataSource.setUrl(environment.getProperty("jdbc.url"));
+        driverManagerDataSource.setUsername(environment.getProperty("jdbc.username"));
+        driverManagerDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        return  driverManagerDataSource;
+    }
+
+    public Properties getHibernateProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        return properties;
     }
 }
