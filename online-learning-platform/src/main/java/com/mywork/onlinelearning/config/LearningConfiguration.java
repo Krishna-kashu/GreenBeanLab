05c75f5fb025180dc2c25180753b1d070a57c4b0
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-05 22:11:49 +0530

    working on spring mvc project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/config/LearningConfiguration.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/config/LearningConfiguration.java
@@ -1,8 +1,14 @@
 package com.mywork.onlinelearning.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
@@ -11,10 +17,18 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@PropertySource("classpath:application.properties")
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.mywork.onlinelearning")
 public class LearningConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
 
     public LearningConfiguration() {
         System.out.println("no-arg constructor in LearningConfiguration");
@@ -47,4 +61,59 @@ public class LearningConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("/images/");
     }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean (){
+
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+
+        localContainerEntityManagerFactoryBean.setPackagesToScan(new String[]{"com.mywork.onlinelearning.entity"});
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        driverManagerDataSource.setUrl(environment.getProperty("jdbc.url"));
+        driverManagerDataSource.setPassword(environment.getProperty("jdbc.password"));
+        driverManagerDataSource.setUsername(environment.getProperty("jdbc.username"));
+
+        return driverManagerDataSource;
+    }
+
+    public Properties getHibernateProperties(){
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        return properties;
+    }
 }
+
+
+/*
+
+create one audit table that shd update whenever the rej table altered
+
+audit table fields:
+auditID
+//admin id
+//admin name
+learnerId
+updated by
+updated on
+created by
+created on
+
+
+ */
