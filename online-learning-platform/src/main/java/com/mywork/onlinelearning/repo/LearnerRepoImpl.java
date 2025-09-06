a24cbb150f01f1ee70b68d4b51758fd24bd93b52
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-06 23:45:10 +0530

    working on online-learning-platform project

--- online-learning-platform/src/main/java/com/mywork/onlinelearning/repo/LearnerRepoImpl.java
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/repo/LearnerRepoImpl.java
@@ -3,6 +3,7 @@ package com.mywork.onlinelearning.repo;
 import com.mywork.onlinelearning.entity.LearnerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ public class LearnerRepoImpl implements LearnerRepo{
 
     private static final Logger log = LoggerFactory.getLogger(LearnerRepoImpl.class);
 
+    @Autowired
     private EntityManagerFactory emf;
 
     @Override
