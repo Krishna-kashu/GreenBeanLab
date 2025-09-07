2806e2261be369e327ea45e0181e8d2ff720ead8
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-07 23:30:17 +0530

    working on online-learning-platform project

---
+++ online-learning-platform/src/main/java/com/mywork/onlinelearning/controller/AuditController.java
@@ -0,0 +1,29 @@
+package com.mywork.onlinelearning.controller;
+
+
+import com.mywork.onlinelearning.entity.LearnerAuditEntity;
+import com.mywork.onlinelearning.repo.AuditRepo;
+import com.mywork.onlinelearning.repo.AuditRepoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class AuditController {
+
+    @Autowired
+    private AuditRepoImpl auditRepo;
+
+    @GetMapping("allAudit")
+    public String viewAllAudits(Model model) {
+        System.out.println("viewAllAudits method in AuditController");
+        List<LearnerAuditEntity> audits = auditRepo.findAllAudits();
+        model.addAttribute("audits", audits);
+        return "audit";
+    }
+}
