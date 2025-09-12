d7aa82692a05bbb2e6725137f61c42ffcd777117
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-12 22:13:31 +0530

    working on spring jpa project

--- user-management/src/main/java/com/mywork/usermanagement/entity/UserEntity.java
+++ user-management/src/main/java/com/mywork/usermanagement/entity/UserEntity.java
@@ -1,17 +1,25 @@
 package com.mywork.usermanagement.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Entity
 @Data
 @Table(name = "user_details")
+@Where(clause = "is_deleted = false")
 @NamedQueries({
         @NamedQuery(name = "allUser", query = "select u from UserEntity u"),
-        @NamedQuery(name = "deleteById", query = "delete UserEntity u where u.userId=:id"),
         @NamedQuery(name = "updateEntity", query = "update UserEntity u set u.userName=:userName, u.gender=:gender, " +
                 "u.age=:age, u.phoneNumber=:phoneNumber, u.email=:email where userId=:id")
 })
@@ -25,6 +33,9 @@ public class UserEntity {
     @Column(name = "user_name")
     private String userName;
 
+    @Column(name = "is_deleted")
+    private boolean deleted = false;
+
     @Column(name = "gender")
     private String gender;
 
@@ -36,4 +47,53 @@ public class UserEntity {
 
     @Column(name = "email")
     private String  email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<AuditInfoEntity> audits = new HashSet<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private UserProfileEntity profile;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<RoleEntity> roles = new ArrayList<>();
+
+    @PrePersist
+    public void onPrePersist() {
+        AuditInfoEntity audit = new AuditInfoEntity();
+        audit.setUser(this);
+        audit.setUpdatedBy(this.userName);
+        audit.setUpdatedAt(LocalDateTime.now());
+        audits.add(audit);
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        AuditInfoEntity audit = new AuditInfoEntity();
+        audit.setUser(this);
+        audit.setUpdatedBy(this.userName);
+        audit.setUpdatedAt(LocalDateTime.now());
+        audits.add(audit);
+    }
+
+    @PreRemove
+    public void onPreRemove() {
+        AuditInfoEntity audit = new AuditInfoEntity();
+        audit.setUser(this);
+        audit.setUpdatedBy(this.userName);
+        audit.setUpdatedAt(LocalDateTime.now());
+        audits.add(audit);
+    }
+
+
 }
