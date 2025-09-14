20d19dd3ed79b25b474263a60614c652915ecb03
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-14 22:13:09 +0530

    working on dairy-360 project front end

---
+++ dairy-360/src/main/java/com/mywork/dairy360/config/DairyConfiguration.java
@@ -0,0 +1,90 @@
+package com.mywork.dairy360.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@PropertySource("classpath:application.properties")
+@Configuration
+@ComponentScan(basePackages = "com.mywork.dairy360")
+@EnableWebMvc
+public class DairyConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
+
+    public DairyConfiguration(){
+        System.out.println("no-arg constructor of DairyConfiguration");
+    }
+
+    @Bean
+    public InternalResourceViewResolver resourceViewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("/uploads/");
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/images/");
+
+        registry.addResourceHandler("/data/**")
+                .addResourceLocations("/data/");
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(){
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+
+        localContainerEntityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+
+        localContainerEntityManagerFactoryBean.setPackagesToScan(new  String[]{"com.mywork.dairy360.entity"});
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+
+        driverManagerDataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        driverManagerDataSource.setUrl(environment.getProperty("jdbc.url"));
+        driverManagerDataSource.setUsername(environment.getProperty("jdbc.username"));
+        driverManagerDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        return  driverManagerDataSource;
+    }
+
+    public Properties getHibernateProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        return properties;
+    }
+}
