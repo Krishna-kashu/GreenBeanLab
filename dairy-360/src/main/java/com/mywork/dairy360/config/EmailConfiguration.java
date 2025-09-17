31f81c189cf42a9b49196cc019bdb8b221a53917
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-17 13:59:08 +0530

    working on dairy-360 project front end

---
+++ dairy-360/src/main/java/com/mywork/dairy360/config/EmailConfiguration.java
@@ -0,0 +1,28 @@
+package com.mywork.dairy360.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        System.out.println("mailSender method");
+
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("krishnavenin8192@gmail.com");
+        javaMailSender.setPassword("rbye rgxn hunq ljlv");
+
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        return javaMailSender;
+    }
+}
