c54afab6f29d697e600a5043489a1f655c6f8eef
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-19 22:06:00 +0530

    working on dairy-360 project front end

---
+++ dairy-360/src/main/java/com/mywork/dairy360/controller/AuditController.java
@@ -0,0 +1,47 @@
+package com.mywork.dairy360.controller;
+
+
+import com.mywork.dairy360.dto.AuditDTO;
+import com.mywork.dairy360.entity.AuditEntity;
+import com.mywork.dairy360.service.AdminServiceImpl;
+import com.mywork.dairy360.service.AuditServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/")
+public class AuditController {
+
+
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @Autowired
+    private AuditServiceImpl auditService;
+
+    @GetMapping("/audit-info")
+    public String auditInfoPage(Model model) {
+        List<AuditEntity> auditEntities = auditService.getAllAudits();
+
+        List<AuditDTO> auditList = auditEntities.stream().map(audit -> {
+            AuditDTO dto = new AuditDTO();
+            dto.setId(audit.getId().longValue());
+            if (audit.getAdmin() != null) {
+                dto.setAdmin(audit.getAdmin());
+            }
+            dto.setAdminName(audit.getAdminName());
+            dto.setLoginTime(audit.getLoginTime());
+            dto.setLogoutTime(audit.getLogoutTime());
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("auditList", auditList);
+        return "auditInfo";
+    }
+}
