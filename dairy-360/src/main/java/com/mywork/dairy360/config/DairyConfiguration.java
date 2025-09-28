423f11c80a3088578beaf2b7cc039b5fa5cb36cd
Krishna-kashu <krishnavenin8192@gmail.com>
2025-09-28 11:55:12 +0530

    working on dairy-360 Spring MVC project

--- dairy-360/src/main/java/com/mywork/dairy360/config/DairyConfiguration.java
+++ dairy-360/src/main/java/com/mywork/dairy360/config/DairyConfiguration.java
@@ -1,6 +1,7 @@
 package com.mywork.dairy360.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -32,6 +36,7 @@ public class DairyConfiguration implements WebMvcConfigurer {
 
     @Bean
     public InternalResourceViewResolver resourceViewResolver(){
+        System.out.println("resourceViewResolver method");
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
         internalResourceViewResolver.setPrefix("/");
         internalResourceViewResolver.setSuffix(".jsp");
@@ -41,8 +46,18 @@ public class DairyConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("Configuring resource handlers...");
+
+        String uploadPath = environment.getProperty("file.upload-dir");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("/uploads/");
+                .addResourceLocations("file:" + uploadPath + "/");
 
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("/images/");
@@ -53,6 +68,8 @@ public class DairyConfiguration implements WebMvcConfigurer {
 
     @Bean
     public LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean(){
+        System.out.println("localContainerEntityManagerFactoryBean method in dairyConfiguration");
+
         LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         localContainerEntityManagerFactoryBean.setDataSource(dataSource());
 
@@ -69,6 +86,7 @@ public class DairyConfiguration implements WebMvcConfigurer {
     @Bean
     public DataSource dataSource() {
 
+        System.out.println("dataSource method in DairyConfiguration");
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 
         driverManagerDataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
@@ -80,11 +98,26 @@ public class DairyConfiguration implements WebMvcConfigurer {
     }
 
     public Properties getHibernateProperties(){
+        System.out.println("getHibernateProperties method DairyConfiguration");
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
         properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
         properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
         return properties;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        System.out.println("bCryptPasswordEncoder bean created");
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver()
+    {
+        System.out.println("MultipartResolver method");
+        return new StandardServletMultipartResolver();
     }
 }
