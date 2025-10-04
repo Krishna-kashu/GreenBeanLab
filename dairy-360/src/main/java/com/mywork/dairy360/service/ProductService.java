0d8795ccfaf1a03bc92ee203ecbedf948aae6748
Krishna-kashu <krishnavenin8192@gmail.com>
2025-10-04 11:17:30 +0530

    working on dairy-360 Spring MVC project

---
+++ dairy-360/src/main/java/com/mywork/dairy360/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.mywork.dairy360.service;
+
+import com.mywork.dairy360.dto.AdminDTO;
+import com.mywork.dairy360.dto.ProductDTO;
+import java.util.List;
+
+public interface ProductService {
+
+    boolean saveProduct(ProductDTO dto, AdminDTO adminDTO);
+
+    ProductDTO getProductById(Integer id);
+
+    boolean updateProduct(ProductDTO dto, AdminDTO adminDTO);
+
+    boolean softDeleteProduct(Integer id, AdminDTO adminDTO);
+
+    List<ProductDTO> getAllActiveProducts(int page, int size);
+
+    long countActiveProducts();
+}
