3ab1c3cd9d2e10aa204f9e6ed2c5705ccc0e7e41
Krishna-kashu <krishnavenin8192@gmail.com>
2025-10-08 23:06:44 +0530

    working on dairy-360 Spring MVC project

---
+++ dairy-360/src/main/java/com/mywork/dairy360/controller/CollectorController.java
@@ -0,0 +1,7 @@
+package com.mywork.dairy360.controller;
+
+public class CollectorController {
+    public CollectorController(){
+        System.out.println("no arg constructor of collector");
+    }
+}
