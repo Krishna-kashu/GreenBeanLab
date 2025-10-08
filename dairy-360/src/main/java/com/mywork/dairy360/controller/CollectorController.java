4d317a96cda0c0e890d28f4d121675dca0301477
Krishna-kashu <krishnavenin8192@gmail.com>
2025-10-08 23:08:10 +0530

    working on dairy-360 Spring MVC project

--- dairy-360/src/main/java/com/mywork/dairy360/controller/CollectorController.java
+++ dairy-360/src/main/java/com/mywork/dairy360/controller/CollectorController.java
@@ -4,4 +4,9 @@ public class CollectorController {
     public CollectorController(){
         System.out.println("no arg constructor of collector");
     }
+
+    public String redirectToCollect(){
+        System.out.println("redirecting to milk collect page");
+        return "milk";
+    }
 }
