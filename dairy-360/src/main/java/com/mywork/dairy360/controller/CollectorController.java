f0707d2df641c43a0d8c7baccef144ed5841c7d7
Krishna-kashu <krishnavenin8192@gmail.com>
2025-10-09 23:16:50 +0530

    working on dairy-360 Spring MVC project milk collect feature

--- dairy-360/src/main/java/com/mywork/dairy360/controller/CollectorController.java
+++ dairy-360/src/main/java/com/mywork/dairy360/controller/CollectorController.java
@@ -1,12 +1,45 @@
 package com.mywork.dairy360.controller;
 
+import com.mywork.dairy360.dto.CollectMilkDTO;
+import com.mywork.dairy360.service.MilkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
 public class CollectorController {
-    public CollectorController(){
-        System.out.println("no arg constructor of collector");
+
+    @Autowired
+    private MilkService milkService;
+
+    @GetMapping("/collectMilk")
+    public String showCollectMilkPage(Model model) {
+        model.addAttribute("milkDTO", new CollectMilkDTO());
+        return "collectMilk";
     }
 
-    public String redirectToCollect(){
-        System.out.println("redirecting to milk collect page");
-        return "milk";
+    @PostMapping("/collectMilk")
+    public String collectMilk(@Valid @ModelAttribute("milkDTO") CollectMilkDTO milkDTO,
+                              BindingResult result,
+                              Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("errorMsg", "Please correct the form errors!");
+            return "collectMilk";
+        }
+
+        boolean saved = milkService.validateAndSave(milkDTO);
+        if (saved) {
+            model.addAttribute("successMsg", "Milk collection recorded successfully!");
+            return "collectMilkSuccess";
+        } else {
+            model.addAttribute("errorMsg", "Unable to record milk details. Try again.");
+            return "collectMilkError";
+        }
     }
 }
