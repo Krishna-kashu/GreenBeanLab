c56023f55e20de6fec28bfdff4f2c9fe6c91a348
Krishna-kashu <krishnavenin8192@gmail.com>
2025-10-10 23:20:11 +0530

    working on dairy-360 Spring MVC project milk collect feature

--- dairy-360/src/main/java/com/mywork/dairy360/repo/MilkRepoImpl.java
+++ dairy-360/src/main/java/com/mywork/dairy360/repo/MilkRepoImpl.java
@@ -1,5 +1,49 @@
 package com.mywork.dairy360.repo;
 
+
+import com.mywork.dairy360.entity.CollectMilkEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Repository
 public class MilkRepoImpl implements MilkRepo{
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public boolean save(CollectMilkEntity entity) {
+        entityManager.persist(entity);
+        return true;
+    }
+
+    @Override
+    public CollectMilkEntity findByPhoneNumber(String phoneNumber) {
+        try {
+            Query query = entityManager.createQuery("SELECT m FROM MilkEntity m WHERE m.phoneNumber = :ph");
+            query.setParameter("ph", phoneNumber);
+            return (CollectMilkEntity) query.getSingleResult();
+        } catch (Exception e) {
+            System.out.println("No record found for phone: " + phoneNumber);
+            return null;
+        }
+    }
+
+    @Override
+    public boolean updateTotalAmountById(double totalAmount, int id) {
+        try {
+            Query query = entityManager.createQuery(
+                    "UPDATE MilkEntity m SET m.totalAmount = :amt WHERE m.id = :id");
+            query.setParameter("amt", totalAmount);
+            query.setParameter("id", id);
+            int updated = query.executeUpdate();
+            return updated > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
