cd9b67d6f0f5a059c5d1251622ea0bc73d360519
Krishna-kashu <krishnavenin8192@gmail.com>
2025-10-10 23:20:04 +0530

    working on dairy-360 Spring MVC project milk collect feature

--- dairy-360/src/main/java/com/mywork/dairy360/repo/MilkRepo.java
+++ dairy-360/src/main/java/com/mywork/dairy360/repo/MilkRepo.java
@@ -1,5 +1,12 @@
 package com.mywork.dairy360.repo;
 
+import com.mywork.dairy360.entity.CollectMilkEntity;
+
 public interface MilkRepo {
 
+    boolean save(CollectMilkEntity entity);
+
+    CollectMilkEntity findByPhoneNumber(String phoneNumber);
+
+    boolean updateTotalAmountById(double totalAmount, int id);
 }
